d3e79e29db7fc3f6faa2345a97dc53af9bc24961
Delcio  Ribeiro <delcioribeiro97@gmail.com>
2025-05-11 20:43:01 -0300

    Fim das Condiçoes

---
+++ aula12/ex013.js
@@ -0,0 +1,38 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+    0 = Domingo
+    1 = Segunda 
+    2 = Terça 
+    3 = Quarta
+    4 = uinta
+    5 = Sexta
+    6 = Sabado
+*/
+
+console.log(diaSem)
+switch(diaSem) {
+    case 0:
+        console.log('Dominmgo')
+        break
+    case 1: 
+        console.log('Segunda-Feira')
+        break
+    case 2: 
+        console.log('Terça-Feira')
+        break
+    case 3: 
+        console.log('Quarta-Feira')
+        break
+    case 4: 
+        console.log('Quinta-Feria')
+        break
+    case 5:
+        console.log('Sexta-Feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('Dia Invalido')
+}
